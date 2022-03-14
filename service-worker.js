3fbbf761ac7f3ae37f6f58e8eab01d32cbdb7443
Krzysztof Kaczyński <k.kaczynski97@gmail.com>
2022-03-14 02:24:58 +0100

    feat: add service worker

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+console.log('I am ready to serve 😎');
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import { App } from './App.js';
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js')
+      .then((registration) => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch((error) => {
+        console.log('ServiceWorker registration failed: ', error);
+      })
+  })
+}
